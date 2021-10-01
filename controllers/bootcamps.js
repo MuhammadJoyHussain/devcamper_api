14132b4513bc82ee8aa1b8de93f7e5243be18bb4
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-10-02 01:42:44 +0600

    advance filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,7 +8,17 @@ const Bootcamps = require('../models/Bootcamps');
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = Bootcamps.find(JSON.parse(queryStr));
+
+    console.log(queryStr);
+
+    const bootcamps = await query;
     res.status(200).json({
         success: true,
         count: bootcamps.length,
