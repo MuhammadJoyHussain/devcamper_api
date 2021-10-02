4fe1e30762aa81ccfc8d4731ec75053db345d336
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-10-02 15:24:52 +0600

    course seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamps');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -18,10 +19,13 @@ mongoose.connect(process.env.MONGO_URI, {
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/Courses.json`, 'utf-8'));
+
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
 
         console.log('Data Imported...'.green.inverse);
         process.exit();
@@ -34,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
