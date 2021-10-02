3ab1e481a824f77531527a583bef496959d886e1
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-10-02 23:56:27 +0600

    update and delete route

--- controllers/courses.js
+++ controllers/courses.js
@@ -66,3 +66,42 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
         data: course
     });
 });
+
+// @des     Delete course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    await course.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
+
+// @des     Update course
+// @route   PUT /api/v1/courses/:id
+// @access  Public
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
